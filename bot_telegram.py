c3b95d6ef123721169eeeae89f7607c8d7a88121
jose leonel <90006477+joseleonelmacedo@users.noreply.github.com>
2024-11-18 08:58:16 -0300

    criação de uma especie de menu

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,24 +1,45 @@
 import telebot
 from datetime import datetime
 
+# Token do bot
 token = "7852388462:AAGjxA7nls0cqzxKW81KV-7pMQbB0Rk_ATg"
 
+# Inicializando o bot
 bot = telebot.TeleBot(token)
 
+# Função para verificar mensagens
 def verificar(mensagem):
     return True
 
+# Manipulador de mensagens
 @bot.message_handler(func=verificar)
 def responder(mensagem):
     texto = mensagem.text.lower()
 
     if texto == "ola":
-        bot.reply_to(mensagem, "Opa, aqui é o ZenBot!")
-    elif texto == "que horas são ?":
+        bot.send_message(mensagem.chat.id, "Opa, aqui é o ZenBot!")
+        bot.send_message(
+            mensagem.chat.id, 
+            """
+Escolha uma opção para começar (clique no ícone):
+/opcao1 : dar bom dia
+/opcao2 : falar a hora
+/opcao3 : falar uma cor
+            """
+        )
+
+    if texto =="/opcao1":
+        bot.send_message(mensagem.chat.id, "Bom dia!")
+    
+    elif texto =="/opcao2":
         hora_atual = datetime.now().strftime("%H:%M:%S")
-        bot.reply_to(mensagem, f"A hora exata agora é: {hora_atual}")
-    elif texto == "fale uma cor":
-        bot.reply_to(mensagem, "Vermelho")
+        bot.send_message(mensagem.chat.id,f"A hora exata agora é: {hora_atual}")
+
+    elif texto == "/opcao3":
+        bot.send_message(mensagem.chat.id, "vermelho!")
+
+
+
 
 try:
     bot.polling()
